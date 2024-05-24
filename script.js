fdc8b9aec09de8359af1da07923bfce75af303c8
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-05-24 16:48:41 -0700

    background image changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 1000;
-canvas.height = 100;
+canvas.height = 1000;
 document.body.appendChild(canvas);
 
 //Images that the game need//
@@ -13,7 +13,7 @@ let backgroundImg = new Image();
 backgroundImg.onload = function (){
     backgroundReady = true;
 };
-backgroundImg.src = "images/backgroundSand.png";
+backgroundImg.src = "images/backgroundSand2.png";
 //**********Character Image**********//
 let girlReady = false;
 let girlImg = new Image();
@@ -29,11 +29,29 @@ crabImg.onload = function(){
 };
 crabImg.src ="images/crab.gif"
 //**********Palm Tree Image**********//
-let palmTreeReady = false;
-let palmTreeImg =  new Image();
-palmTreeImg.onload =  function (){
-    palmTreeReady = true;
+// let palmTreeReady = false;
+// let palmTreeImg =  new Image();
+// palmTreeImg.onload =  function (){
+//     palmTreeReady = true;
+// };
+// palmTreeImg.src="images/palmTree.png";
+
+//**********The Main Game Loop**********//
+let main = function() {
+    render ();
+    requestAnimationFrame(main);
 };
-palmTreeImg.src="images/palmTree.png";
+
+//**********To Render the Game**********/
+let render = function(){
+    if (backgroundReady) {
+        console.log('here2');
+        ctx.drawImage(backgroundImg,0,0);
+    }
+};
+//**********To play the Game*********//
+let then = Date.now();
+// reset();
+main(); // To call the main game loop//
 
 
